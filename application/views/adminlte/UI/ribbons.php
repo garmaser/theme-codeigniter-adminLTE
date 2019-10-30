039ae31ae7d4b5ba84b3ea86c24ca74f25a85028
Diego Reynaldo <reydi.reynaldo@gmail.com>
2019-10-29 23:27:44 -0400

    terminado

---
+++ application/views/adminlte/UI/ribbons.php
@@ -0,0 +1,96 @@
+
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Ribbons</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-sm-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 180px">
+                      <div class="ribbon-wrapper">
+                        <div class="ribbon bg-primary">
+                          Ribbon
+                        </div>
+                      </div>
+                      Ribbon Default <br />
+                      <small>.ribbon-wrapper.ribbon-lg .ribbon</small>
+                    </div>
+                  </div>
+                  <div class="col-sm-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 180px">
+                      <div class="ribbon-wrapper ribbon-lg">
+                        <div class="ribbon bg-info">
+                          Ribbon Large
+                        </div>
+                      </div>
+                      Ribbon Large <br />
+                      <small>.ribbon-wrapper.ribbon-lg .ribbon</small>
+                    </div>
+                  </div>
+                  <div class="col-sm-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 180px">
+                      <div class="ribbon-wrapper ribbon-xl">
+                        <div class="ribbon bg-secondary">
+                          Ribbon Extra Large
+                        </div>
+                      </div>
+                      Ribbon Extra Large <br />
+                      <small>.ribbon-wrapper.ribbon-xl .ribbon</small>
+                    </div>
+                  </div>
+                </div>
+                <div class="row mt-4">
+                  <div class="col-sm-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 180px">
+                      <div class="ribbon-wrapper ribbon-lg">
+                        <div class="ribbon bg-success text-lg">
+                          Ribbon
+                        </div>
+                      </div>
+                      Ribbon Large <br /> with Large Text <br />
+                      <small>.ribbon-wrapper.ribbon-lg .ribbon.text-lg</small>
+                    </div>
+                  </div>
+                  <div class="col-sm-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 180px">
+                      <div class="ribbon-wrapper ribbon-xl">
+                        <div class="ribbon bg-warning text-lg">
+                          Ribbon
+                        </div>
+                      </div>
+                      Ribbon Extra Large <br /> with Large Text <br />
+                      <small>.ribbon-wrapper.ribbon-xl .ribbon.text-lg</small>
+                    </div>
+                  </div>
+                  <div class="col-sm-4">
+                    <div class="position-relative p-3 bg-gray" style="height: 180px">
+                      <div class="ribbon-wrapper ribbon-xl">
+                        <div class="ribbon bg-danger text-xl">
+                          Ribbon
+                        </div>
+                      </div>
+                      Ribbon Extra Large <br /> with Extra Large Text <br />
+                      <small>.ribbon-wrapper.ribbon-xl .ribbon.text-xl</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  
---
+++ application/views/adminlte/forms/editors.php
@@ -0,0 +1,42 @@
+
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card card-outline card-info">
+            <div class="card-header">
+              <h3 class="card-title">
+                Bootstrap WYSIHTML5
+                <small>Simple and fast</small>
+              </h3>
+              <!-- tools box -->
+              <div class="card-tools">
+                <button type="button" class="btn btn-tool btn-sm" data-card-widget="collapse" data-toggle="tooltip"
+                        title="Collapse">
+                  <i class="fas fa-minus"></i></button>
+                <button type="button" class="btn btn-tool btn-sm" data-card-widget="remove" data-toggle="tooltip"
+                        title="Remove">
+                  <i class="fas fa-times"></i></button>
+              </div>
+              <!-- /. tools -->
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body pad">
+              <div class="mb-3">
+                <textarea class="textarea" placeholder="Place some text here"
+                          style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+              </div>
+              <p class="text-sm mb-0">
+                Editor <a href="https://github.com/bootstrap-wysiwyg/bootstrap3-wysiwyg">Documentation and license
+                information.</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- /.col-->
+      </div>
+      <!-- ./row -->
+    </section>
+    <!-- /.content -->
+  
